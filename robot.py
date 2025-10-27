ab63c1f5d4347f28925ccec80201afb192c0c550
AimanFaris8900 <aimanfaris.af@gmail.com>
2025-10-27 18:03:21 +0800

    Commit 27 Oct 2025

--- robot.py
+++ robot.py
@@ -8,6 +8,13 @@ import websockets
 import json
 import httpx
 
+mongo_client = MongoClient("mongodb://localhost:27017/")
+
+db = mongo_client["robotDB"]
+
+robot_col = db['robots']
+poi_col = db['poi']
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     mongo_client = MongoClient("mongodb://localhost:27017/")
@@ -36,8 +43,21 @@ async def main_hello():
         data = r.json()
         return data
     
-@router.get("/set/poi/{name}")
-async def set_poi_location(name):
+@router.get("/get/poi_list")
+async def get_poi_list():
+    poi_list = []
+
+    poi_data = poi_col.find()
+    for poi in poi_data:
+        poi["_id"] = str(poi["_id"])
+        poi_list.append(poi)
+
+    print(poi_list)
+
+    return poi_list
+    
+@router.get("/set/poi")
+async def set_poi_location(name: str):
     async with httpx.AsyncClient() as client:
         url = EDGE_URL+"/edge/v1/robot/position"
         try:
@@ -47,8 +67,8 @@ async def set_poi_location(name):
             coord = data["pos"]
             ori = data['ori']
 
-            poi = {"name" : name, "data" : {"target_x" : coord[0], "target_y" : coord[1], "target_ori" : ori}}
-            robot_col.insert_one(poi)
+            poi = {"name" : name, "data" : {"target_x" : coord[0], "target_y" : coord[1], "target_ori" : ori}, "time_created" : round(time.time(),1)}
+            poi_col.insert_one(poi)
 
             msg = f"POI named {name} successfully saved! : {poi['data']}"
             return msg
@@ -56,15 +76,30 @@ async def set_poi_location(name):
         except Exception as e:
             return f"{e}: Error retrieving position"
         
-@router.post("/move/poi/{name}")
-async def go_to_poi(name):
+@router.get("/move/charge")
+async def go_to_charge():
+    async with httpx.AsyncClient() as client:
+        url = EDGE_URL+"/edge/v1/robot/move/charge"
+        r = await client.get(url)
+        r.raise_for_status()
+        data = r.json()
+
+        return
+        
+@router.get("/move/poi")
+async def go_to_poi(name: str):
     header = {
         "Content-Type" : "application/json"
     }
 
-    data = robot_col.find({"name" : name})
+    data = poi_col.find_one({"name" : name})
+    print("DATA: ", data)
     payload = data["data"]
 
     async with httpx.AsyncClient() as client:
-        url = EDGE_URL+"/move/poi"
+        url = EDGE_URL+"/edge/v1/robot/move/poi"
         r = await client.post(url, headers=header, json=payload)
+        r.raise_for_status()
+        data = r.json()
+
+        return data
